7ee3c9e097f28931556f597401fce8163879c803
gilangkurniar <gilkr20@gmail.com>
2023-12-26 04:55:26 +0700

    minor update

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -7,13 +7,19 @@ window.onload = function () {
 
     icon.onclick = function () {
         const nav = document.querySelector(".navbar-toggler")
+        const sun = document.getElementById("sun")
+        const moon = document.getElementById("moon")
 
         document.body.classList.toggle("dark")
         if (document.body.classList.contains("dark")) {
             nav.classList.toggle("navbar-dark")
+            sun.src = ""
+            moon.src = "/img/moon.png"
         } else {
             nav.classList.remove("navbar-dark")
             nav.classList.toggle("navbar-light")
+            moon.src = ""
+            sun.src = "/img/sun.png"
         }
     }
 }
@@ -24,12 +30,12 @@ const NavbarComponent = () => {
             <Container>
                 <Navbar.Brand href="/" className='fw-semibold'>GKR</Navbar.Brand>
                 <div className="nightmode d-flex">
-                    <img src="/img/sun.png" className='me-2' width="22px" />
+                    <img src="/img/sun.png" id='sun' className='me-2' width="22px" />
                     <Form.Check // prettier-ignore
                         type="switch"
                         id="toggle-icon"
                     />
-                    <img src="/img/moon.png" width="20px" />
+                    <img src="" id='moon' className='mx-1' height="18px" />
                 </div>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
