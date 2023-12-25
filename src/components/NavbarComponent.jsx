b88e1e489caf08c4773b5651b053adf2e31f24d8
gilangkurniar <gilkr20@gmail.com>
2023-12-26 05:52:38 +0700

    bug fix

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -2,26 +2,43 @@ import { React, useEffect, useState, } from 'react'
 import { Container, Form, Nav, Navbar } from "react-bootstrap"
 import { NavLink } from 'react-router-dom'
 
-const night = () => {
-    let icon = document.getElementById("toggle-icon")
+const DarkMode = () => {
 
-    icon.onclick = function () {
-        const nav = document.querySelector(".navbar-toggler")
+    const setDarkMode = () => {
         const sun = document.getElementById("sun")
         const moon = document.getElementById("moon")
 
-        document.body.classList.toggle("dark")
-        if (document.body.classList.contains("dark")) {
-            nav.classList.toggle("navbar-dark")
-            sun.src = ""
-            moon.src = "/img/moon.png"
-        } else {
-            nav.classList.remove("navbar-dark")
-            nav.classList.toggle("navbar-light")
-            moon.src = ""
-            sun.src = "/img/sun.png"
-        }
+        document.querySelector("body").classList.toggle("dark")
+
+        sun.src = ""
+        moon.src = "/img/moon.png"
     }
+
+    const setlightMode = () => {
+        const sun = document.getElementById("sun")
+        const moon = document.getElementById("moon")
+
+        document.querySelector("body").classList.remove("dark")
+
+        moon.src = ""
+        sun.src = "/img/sun.png"
+    }
+
+    const toggleTheme = (e) => {
+        if (e.target.checked) setDarkMode()
+        else setlightMode()
+    }
+
+    return (
+        <div className="nightmode d-flex">
+            <img src="/img/sun.png" id='sun' className='me-2' width="22px" />
+            <Form.Check
+                type="switch"
+                onClick={toggleTheme}
+            />
+            <img src="" id='moon' className='mx-1' height="18px" />
+        </div>
+    )
 }
 
 const NavbarComponent = () => {
@@ -29,15 +46,7 @@ const NavbarComponent = () => {
         <Navbar expand="lg" className="">
             <Container>
                 <Navbar.Brand href="/" className='fw-semibold'>GKR</Navbar.Brand>
-                <div className="nightmode d-flex">
-                    <img src="/img/sun.png" id='sun' className='me-2' width="22px" />
-                    <Form.Check
-                        type="switch"
-                        id="toggle-icon"
-                        onClick={night}
-                    />
-                    <img src="" id='moon' className='mx-1' height="18px" />
-                </div>
+                <DarkMode />
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
