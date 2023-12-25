edfd37f00e8a6e90c7e0b11a47207f2baf6fa571
gilangkurniar <gilkr20@gmail.com>
2023-12-26 06:35:51 +0700

    minor up

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
         <ul className="list text-center">
           <li>
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/2560px-Gmail_icon_%282020%29.svg.png" width="20px" />
-            <Link to="https://mail.google.com/mail/?view=cm&source=mailto&to=[gilkr20@gmail.com]" target='_blank'>gilangkurniarmdhn@gmail.com</Link>
+            <Link to="https://mail.google.com/mail/?view=cm&source=mailto&to=[gilangkurniarmdhn@gmail.com]" target='_blank'>gilangkurniarmdhn@gmail.com</Link>
           </li>
           <li>
             <img src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Instagram_colored_svg_1-512.png" width="20px" />
