7a9afe1264ea714d0b2d2389ebf4a7362faa0233
gilangkurniar <gilkr20@gmail.com>
2023-12-30 05:56:33 +0700

    ui update and bug fix

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -4,39 +4,25 @@ import { NavLink } from 'react-router-dom'
 
 const DarkMode = () => {
 
-    const setDarkMode = () => {
-        const sun = document.getElementById("sun")
-        const moon = document.getElementById("moon")
+    const setMode = () => {
+        const body = document.querySelector("body")
+        const icon = document.getElementById("mode")
+        body.classList.toggle("dark")
 
-        document.querySelector("body").classList.toggle("dark")
-
-        sun.src = ""
-        moon.src = "/img/moon.png"
-    }
-
-    const setlightMode = () => {
-        const sun = document.getElementById("sun")
-        const moon = document.getElementById("moon")
-
-        document.querySelector("body").classList.remove("dark")
-
-        moon.src = ""
-        sun.src = "/img/sun.png"
+        if (body.classList.contains("dark")) {
+            icon.src = "/img/moon.png"
+        } else {
+            icon.src = "/img/sun.png"
+        }
     }
 
     const toggleTheme = (e) => {
-        if (e.target.checked) setDarkMode()
-        else setlightMode()
+        if (e) setMode()
     }
 
     return (
-        <div className="nightmode d-flex">
-            <img src="/img/sun.png" id='sun' className='me-2' width="22px" />
-            <Form.Check
-                type="switch"
-                onClick={toggleTheme}
-            />
-            <img src="" id='moon' className='mx-1' height="18px" />
+        <div className="d-flex">
+            <img src="/img/sun.png" onClick={toggleTheme} id='mode' width="22px" />
         </div>
     )
 }
